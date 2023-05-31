3bd1328229bcdfe7b850a3fbad14e56a0f68e917
OnurArk <onurark11@gmail.com>
2023-06-01 01:24:37 +0300

    cursor following buttons

--- components/ui/button/button.tsx
+++ components/ui/button/button.tsx
@@ -1,18 +1,50 @@
-import { FC, ComponentPropsWithRef, forwardRef } from 'react';
+import {
+  FC,
+  ComponentPropsWithRef,
+  forwardRef,
+  MouseEvent,
+  CSSProperties,
+  useState,
+} from 'react';
 
 import styles from './button.module.css';
 
 type PropsButton = ComponentPropsWithRef<'button'>;
 
+let styleLight: CSSProperties;
+
 const Button: FC<PropsButton> = forwardRef((props, ref) => {
+  const [lightLeft, setLightLeft] = useState<any>();
+
+  const handleMouseMove = (event: MouseEvent<HTMLButtonElement>) => {
+    const { clientX } = event;
+
+    const buttonRect = event.currentTarget.getBoundingClientRect();
+    const buttonLeft = buttonRect.left;
+
+    const newLightLeft = clientX - buttonLeft;
+    console.log(newLightLeft);
+    setLightLeft(newLightLeft);
+  };
+
+  const styleLight: CSSProperties = {
+    left: lightLeft,
+  };
+
+  const handleOnMouseLeave = () => {
+    setLightLeft(null);
+  };
+
   return (
     <button
       onClick={props.onClick}
       className={`${props.className} ${styles.btn}`}
       ref={ref}
+      onMouseMove={handleMouseMove}
+      onMouseLeave={handleOnMouseLeave}
     >
       {props.children}
-      <div className={styles.bottomLight} />
+      <div className={styles.bottomLight} style={styleLight} />
     </button>
   );
 });
